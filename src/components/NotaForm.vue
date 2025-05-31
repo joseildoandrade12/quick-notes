0f9b88c5d25f14a89eef78ce3f8096d40c390548
Joseildo <joseildoandrade1405@gmail.com>
2025-05-31 17:56:10 -0300

    feature: create modal for create note

--- src/components/NotaForm.vue
+++ src/components/NotaForm.vue
@@ -1,5 +1,26 @@
 <script setup>
+import CreateForm from "./elements/CreateForm.vue";
+import Button from "./elements/Button.vue";
 </script>
 
 <template>
+  <div class="container-modal">
+    <CreateForm title="Title" height="50" limit="60" />
+    <CreateForm title="Content" height="220" limit="400" />
+    <Button content="Save" width="240" />
+  </div>
 </template>
+
+<style scoped>
+.container-modal {
+  width: max-content;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  margin-top: 36px;
+  background-color: #fff;
+  padding: 20px;
+  border: 1px solid #111827;
+  border-radius: 20px;
+  position: absolute;
+  right: 120px;
+}
+</style>
---
+++ src/components/elements/CreateForm.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  height: [Number, String],
+  limit: [Number, String],
+});
+</script>
+
+<template>
+  <div class="container-form">
+    <label :for="title"> {{ title }} </label>
+    <textarea :style="{ height: height + 'px' }" :id="title" :maxlength="limit"></textarea>
+  </div>
+</template>
+
+<style scoped>
+.container-form {
+  display: flex;
+  flex-direction: column;
+}
+label {
+  font-size: 2.25rem;
+  margin-bottom: 36px;
+  color: #111827;
+}
+textarea {
+  resize: none;
+  width: 580px;
+  outline: none;
+  padding: 12px;
+  font-size: 1.25rem;
+  border: 1px solid #111827;
+  margin-bottom: 36px;
+  border-radius: 20px;
+}
+</style>
