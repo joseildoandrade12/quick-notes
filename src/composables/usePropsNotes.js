e071593853f67b6e29f884f069f9b6d5cb23834e
Joseildo <joseildoandrade1405@gmail.com>
2025-08-30 15:55:28 -0300

    feat: addition countId for cards

--- src/composables/usePropsNotes.js
+++ src/composables/usePropsNotes.js
@@ -1,13 +1,15 @@
 import { ref } from "vue";
 const objData = ref([]);
+const countId = ref(0);
 export function usePropsNotes() {
-  const pushDataArray = (title, content, date, id) => {
+  const pushDataArray = (title, content, date) => {
     objData.value.push({
       title,
       content,
       date,
-      id,
+      id: countId.value,
     });
+    countId.value++;
   };
   return { objData, pushDataArray };
 }
